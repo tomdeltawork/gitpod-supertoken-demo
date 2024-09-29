724e0bffdf0254a447f4b1caf8c97017d78867c1
tomdeltawork@gmail.com <tomdeltawork@gmail.com>
2024-09-29 14:46:10 +0000

    Modify deployment file

--- start.sh
+++ start.sh
@@ -47,8 +47,8 @@ fi
 
 echo "Successfully retrieved JWT token"
 
-# 啟動 Docker 堆疊
-START_RESPONSE=$(curl -s -X POST "$PORTAINER_URL/api/stacks/$STACK_ID/start" \
+# 啟動 Docker 堆疊，並添加 endpointId 參數
+START_RESPONSE=$(curl -s -X POST "$PORTAINER_URL/api/stacks/$STACK_ID/start?endpointId=$ENDPOINT_ID" \
     -H "Authorization: Bearer $TOKEN")
 
 # 檢查是否成功啟動堆疊
