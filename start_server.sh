caa277cbd38b50f8a3b7b8be59bda50b8d9099c9
tomdeltawork@gmail.com <tomdeltawork@gmail.com>
2024-10-08 05:48:08 +0000

    adjust automatic script

--- start_server.sh
+++ start_server.sh
@@ -2,13 +2,13 @@
 
 # 用於啟動supertoken*************************************************
 # 初始化supertoken專案
-cd //workspace/gitpod-supertoken-demo/codeWorkspace/supertoken-demo/backend || exit 1  # 如果目錄不存在，則退出
-npm i
+#cd //workspace/gitpod-supertoken-demo/codeWorkspace/supertoken-demo/backend || exit 1  # 如果目錄不存在，則退出
+#npm i
 
-cd //workspace/gitpod-supertoken-demo/codeWorkspace/supertoken-demo/frontend || exit 1  # 如果目錄不存在，則退出
-npm i
+#cd //workspace/gitpod-supertoken-demo/codeWorkspace/supertoken-demo/frontend || exit 1  # 如果目錄不存在，則退出
+#npm i
 
 # 切換到目錄
-cd //workspace/gitpod-supertoken-demo/codeWorkspace/supertoken-demo || exit 1  # 如果目錄不存在，則退出
+#cd //workspace/gitpod-supertoken-demo/codeWorkspace/supertoken-demo || exit 1  # 如果目錄不存在，則退出
 
-npm run start
+#npm run start
